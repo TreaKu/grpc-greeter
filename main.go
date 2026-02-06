6a3845d9db358108c208c4d79628a1bbf8675d72
TreaKu <treaku@gmail.com>
2026-02-06 18:10:48 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "example/helloworld"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{
+		Message: "Hello " + req.Name,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreeterServer(grpcServer, &server{})
+
+	log.Println("gRPC server listening on :50051")
+	grpcServer.Serve(lis)
+}
